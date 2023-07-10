5e8cccc51078fe109756a1d694f8c6ff2e665c10
Igor Yaitsky <igoryaitsky@Igor-Yaitsky-MacBook-Pro.local>
2023-07-10 14:24:31 +0300

    main. fix demo animation to loader

--- components/ui/loader.vue
+++ components/ui/loader.vue
@@ -18,8 +18,38 @@
   justify-content: center;
 
   .ui-logo {
-    img {
-      height: 80px;
+    animation: loader 3s ease-in-out 0s infinite normal backwards;
+  }
+
+  @keyframes loader {
+    0%,
+    100% {
+      transform: rotate(0deg);
+      transform-origin: 50% 100%;
+    }
+
+    10% {
+      transform: rotate(2deg);
+    }
+
+    20%,
+    40%,
+    60% {
+      transform: rotate(-4deg);
+    }
+
+    30%,
+    50%,
+    70% {
+      transform: rotate(4deg);
+    }
+
+    80% {
+      transform: rotate(-2deg);
+    }
+
+    90% {
+      transform: rotate(2deg);
     }
   }
 }
